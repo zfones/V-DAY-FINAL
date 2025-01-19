66e3523b05c1a2c1b0035d8bd5de26dec6fa14db
zfones <zachfones@gmail.com>
2025-01-19 17:09:22 -0500

    Add App component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ValentinesJewelryStore from './components/ValentinesJewelryStore';
+
+function App() {
+  return (
+    <ValentinesJewelryStore />
+  );
+}
+
+export default App;
